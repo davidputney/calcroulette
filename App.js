cea2b2b5a59b657050e403163cebfdab3ec85be9
David Putney <david.putney@cantina.co>
2020-06-29 08:43:30 -0400

    Adds various components and stuff to learn how it works.

--- App.js
+++ App.js
@@ -1,21 +1,116 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, Button, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback } from 'react-native';
 
-export default function App() {
+
+const TextFoo=({ textVal }) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <Text>{`${textVal}`}</Text>
+  )
 }
+
+const ViewFoo=({ textVal }) => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <TextFoo textVal = { textVal } />
+    </View>  
+  )
+}
+
+const ButtonFoo=() => {
+  return (
+      <Button
+        onPress={() => {
+          alert('You tapped the button!');
+        }}
+        title="Press Me Here"
+        color="#841584"
+      />
+    )
+  }
+
+ const TouchFoo=({ touch, buttonText, buttonStyle }) => {
+  return (
+    <TouchableHighlight 
+      onPress={touch}
+      underlayColor="white"
+    >
+      <View style={buttonStyle}>
+        <Text style={styles.buttonText}>{`${buttonText}`}</Text>
+      </View>
+    </TouchableHighlight>
+  )
+ } 
+
+  
+export default class HelloWorldApp extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { textVal: "Hello world var yyy" }
+    this._onPressButton = this._onPressButton.bind(this)
+  }  
+
+
+  _onPressButton() {
+    alert('You tapped the button!')
+  }
+
+  _onLongPressButton() {
+    alert('You long-pressed the button!')
+  }
+
+
+  render() {
+    return ( 
+      <View style={styles.container}>
+        <TouchFoo 
+          touch = { this._onPressButton} 
+          buttonText = { "Button Text" } 
+          buttonStyle = { styles.button }
+        />
+        <TouchFoo 
+          touch = { this._onPressButton} 
+          buttonText = { "Button Text Too" } 
+          buttonStyle = { styles.buttonAlt }
+        />
+      </View>
+    );
+  }
+}
+
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   flex: 1,
+   justifyContent: 'center',
+   backgroundColor: "#d0d0d0",
+   paddingTop: 60,
+   alignItems: 'center'
   },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  button: {
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#2196F3',
+    borderRadius:3
+  },
+  buttonAlt: {
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#FFA500',
+    borderRadius:3
+  },
+  buttonText: {
+    textAlign: 'center',
+    padding: 20,
+    color: 'white'
+  }
 });
