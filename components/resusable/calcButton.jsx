1960ee4892f84e4a0319c26ac8a801cc5702797a
David Putney <david.putney@cantina.co>
2020-07-10 15:30:43 -0400

    Modifies to add active state.

--- components/resusable/calcButton.jsx
+++ components/resusable/calcButton.jsx
@@ -3,9 +3,10 @@ import { View, Text, TouchableHighlight } from 'react-native';
 import { styles } from "../../styles/styles";
 import { buttons } from "../../styles/buttons";
 
- const CalcButton=({ f, buttonText, textStyle=styles.buttonText, buttonStyle = buttons.button, touchable = buttons.touchable, val }) => {
+ const CalcButton=({ f, buttonText, textStyle=styles.buttonText, buttonStyle = buttons.button, touchable = buttons.touchable, val, active = true }) => {
     const clicky=(e) => {
-      f(e, val)
+      f(e, val, active)
+      console.log(active)
     }
     return (
       <TouchableHighlight 
@@ -13,7 +14,8 @@ import { buttons } from "../../styles/buttons";
         onPress={clicky}
         underlayColor="#d0d0d0"
       >
-        <View style={buttonStyle}>
+        <View style={buttonStyle}
+        >
           <Text 
             style={textStyle}
           >{`${buttonText}`}</Text>
